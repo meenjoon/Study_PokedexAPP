57155f04291f828217c28c9e92914c4880d0b27a
meenjoon <zqudwnsz@naver.com>
2023-01-11 15:04:55 +0900

    Add: RecyclerView 사용을 위한 액티비티 주석 추가

--- app/src/main/kotlin/com/skydoves/pokedex/ui/main/PokemonAdapter.kt
+++ app/src/main/kotlin/com/skydoves/pokedex/ui/main/PokemonAdapter.kt
@@ -32,18 +32,21 @@ class PokemonAdapter : BindingListAdapter<Pokemon, PokemonAdapter.PokemonViewHol
 
   private var onClickedAt = 0L
 
+  //어떤 xml으로 뷰 홀더를 생성할 지 지정
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonViewHolder =
     parent.binding<ItemPokemonBinding>(R.layout.item_pokemon).let(::PokemonViewHolder)
 
+  //뷰 홀더에 데이터 바인딩
   override fun onBindViewHolder(holder: PokemonViewHolder, position: Int) =
     holder.bindPokemon(getItem(position))
 
+  //생성된 뷰 홀더에 값 지정
   inner class PokemonViewHolder constructor(
     private val binding: ItemPokemonBinding
-  ) : RecyclerView.ViewHolder(binding.root) {
+  ) : RecyclerView.ViewHolder(binding.root) { //뷰바인딩을 이용하여 리사이클러뷰 뷰 홀더와 연결
 
     init {
-      binding.root.setOnClickListener {
+      binding.root.setOnClickListener { //뷰 홀더 클릭 시에
         val position = bindingAdapterPosition.takeIf { it != NO_POSITION }
           ?: return@setOnClickListener
         val currentClickedAt = SystemClock.elapsedRealtime()
@@ -54,13 +57,13 @@ class PokemonAdapter : BindingListAdapter<Pokemon, PokemonAdapter.PokemonViewHol
       }
     }
 
-    fun bindPokemon(pokemon: Pokemon) {
+    fun bindPokemon(pokemon: Pokemon) { //데이터 바인딩과 리사이클러뷰 연결
       binding.pokemon = pokemon
-      binding.executePendingBindings()
+      binding.executePendingBindings() // 데이텨 변경이 즉각적으로 수행되어야 하는 경우
     }
   }
 
-  companion object {
+  companion object { //전역 변수처럼 사용 할 수 있지만, 독립된 객체로 생각해야함
     private val diffUtil = object : DiffUtil.ItemCallback<Pokemon>() {
 
       override fun areItemsTheSame(oldItem: Pokemon, newItem: Pokemon): Boolean =
